59cfdc68940aef64f365ff3092b0be8824e75139
thinh2344 <thinh039765@gmail.com>
2026-07-21 13:40:33 +0700

    Add files via upload

---
+++ UserReviewController.java
@@ -0,0 +1,76 @@
+package poly.edu.controller;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+import poly.edu.entity.Review;
+import poly.edu.entity.User;
+import poly.edu.service.ReviewService;
+import poly.edu.entity.Product;
+import poly.edu.service.ProductService;
+
+@Controller
+public class UserReviewController {
+
+	@Autowired
+	ReviewService reviewService;
+	
+	@Autowired
+	ProductService productService;
+	// Lưu review
+	@PostMapping("/review/save")
+	public String saveReview(@RequestParam("productName") String productName, @RequestParam("rating") Integer rating,
+			@RequestParam("comment") String comment, HttpSession session) {
+
+		User user = (User) session.getAttribute("user");
+
+		// CHỐNG NULL
+		if (user == null) {
+			return "redirect:/login";
+		}
+		
+		Review review = new Review();
+
+		Product product = productService.findByName(productName);
+
+		review.setUsername(user.getUsername());
+		review.setFullname(user.getFullName());
+		review.setEmail(user.getEmail());
+
+		review.setProductName(productName);
+
+		review.setRating(rating);
+		review.setComment(comment);
+
+		review.setUser(user);
+		review.setProduct(product);
+
+		
+
+		reviewService.save(review);
+
+		return "redirect:/orders";
+	}
+
+	// Lịch sử review
+	@GetMapping("/reviews")
+	public String myReviews(Model model, HttpSession session) {
+
+		User user = (User) session.getAttribute("user");
+
+		if (user == null) {
+			return "redirect:/login";
+		}
+		
+
+		model.addAttribute("reviews", reviewService.findByUserId(user.getId()));
+
+		return "user/reviews";
+	}
+	// dsjnkjsbfjbjbdjkbajakns
+	
+}
